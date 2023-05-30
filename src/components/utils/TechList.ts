94219d9e03a3b4f8cbb4c9fd781d765aee0e5aae
SergioYepes <elgatobanban@gmail.com>
2023-05-30 11:31:17 -0500

    feat-css: home css done, background images done

--- src/components/utils/TechList.ts
+++ src/components/utils/TechList.ts
@@ -4,5 +4,6 @@ import spaceCapsule from "../../assets/spaceCapsule.jpg"
 export const techImg=[
     { src: launch  , alt:"launch"},
     { src: spacePort , alt:"spacePort"},
+    
     { src: spaceCapsule , alt:"spaceCapsule"},
 ]
